d0132119da6cc2d70b196863d90540b986e0cab8
Ammonation <harry_a99@hotmail.co.uk>
2019-01-11 13:01:20 +0000

    Updated comments

--- catalogue/BetterBasket.java
+++ catalogue/BetterBasket.java
@@ -3,8 +3,6 @@ package catalogue;
 import java.io.Serializable;
 
 /**
- * Write a description of class BetterBasket here.
- * 
  * @author  Harry Ammon 
  * @version 1.0
  */
@@ -13,25 +11,25 @@ public class BetterBasket extends Basket implements Serializable
   private static final long serialVersionUID = 1L;
   private int theOrderNum = 0; // order number
   
-  // You need to add code here
   /**
    * New constructor
-   * Will create a hashmap to store products in the basket in
-   *
    */
   public BetterBasket() {
-	  //HashMap<String, Product> basket = new HashMap<String, Product>();
 	  theOrderNum = 0;
   }
   
   @Override
   public boolean add(Product pr) {
-	  for(int i = 0;i<super.size();i++) {
+	  for(int i = 0;i<super.size();i++) { // length of Arraylist
+		// If the product number for two products match
 		  if(super.get(i).getProductNum().equals(pr.getProductNum())) {
-			  super.get(i).setQuantity(super.get(i).getQuantity()+1);
+			  // Get the current quantity of that product and increase by one
+			  super.get(i).setQuantity(super.get(i).getQuantity()+1); 
 			  return true;
 		  }
 	  }
+	  // If the product is not found then call original add function to
+	  // to add to the ArrayList
 	  return super.add(pr);
   }
 }
